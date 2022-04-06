fac509feeca7e953b8e8b88316e082457d7495a3
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-06 09:17:01 -0400

    no bodies success

--- testrouting.js
+++ testrouting.js
@@ -1,27 +1,20 @@
 
 function Try_component () {
-var lambdas = {
+    var lambdas = {
+	a: function (_label) {
+	    if (_label === 0) {
+	    } else {
+		this.panic (); 
+	    }
+	},
+	b: function (_label) {
+	    if (_label === 0) {
+	    } else {
+		this.panic (); 
+	    }
+	},
+	null
+    };
+    return (function () { this.lambdas.main (0); });
+}
 
-a: function (_label) {
-if (_label === 0) {
-{}
-
-else {
-this.panic (); 
-}
-}
-},
-
-b: function (_label) {
-if (_label === 0) {
-{}
-
-else {
-this.panic (); 
-}
-}
-},
-null
-}
-return function () { this.lambdas.main (0); }
-}
