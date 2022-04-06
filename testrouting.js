fcd7b3eb9b5f54efb953ce15e77378484376c224
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-06 10:27:23 -0400

    two skewers

--- testrouting.js
+++ testrouting.js
@@ -3,11 +3,12 @@ function Try_component () {
     var lambdas = {
 	Try_component: function (_label) {
 	    if (_label === 0) {
-		if (!this.pred ()) {
-		    return this.dest (5);
-		} else {
-
-		    ;}
+	    } else {
+		this.panic (_label); 
+	    }
+	},
+	skewerb: function (_label) {
+	    if (_label === 0) {
 	    } else {
 		this.panic (_label); 
 	    }
