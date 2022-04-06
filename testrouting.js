c7642262c55c1a705001e5af8deba7b1444ddc4b
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-06 16:22:23 -0400

    tested 4 kinds of skewers A/B/C/D

--- testrouting.js
+++ testrouting.js
@@ -1,27 +1,11 @@
 
-function Try_component () {
-var lambdas = {
-Try_component: function (_label) {
-if (_label === 0) {
-if (!this.xyz ()) {
-return this.skewerb (0);
-} else {
-
-return this.skewerb (0) ();
-;}
-} else {
-this.panic (_label); 
-}
-},
-sb: function (_label) {
-if (_label === 0) {
-this.funcb ();
-} else {
-this.panic (_label); 
-}
-},
-_endoflambdas: null
-};
-return (function () { this.lambdas.Try_component (0); });
+function Sb () {
+    var lambdas = {
+	sb D: function (_label) {
+	    this.panic ("sb", _label); 
+	},
+	_endoflambdas: null
+    };
+    return (function () { this.lambdas.Sb (0); });
 }
 
