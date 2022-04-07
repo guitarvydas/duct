b7ce1b51015e2b3e71401a58c8e6af1548c63712
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-06 22:36:25 -0400

    missing } in else in C:

--- testrouting.js
+++ testrouting.js
@@ -1,43 +1,41 @@
 
 function Try_component () {
     var lambdas = {
-	try_self C: function (_label) {
+	try_self: function (_label) {
 	    if (_label === 0) {
 		return try_self (1);
 	    } else if (_label === 1) {
 
 		return try_self (2);
 	    } else if (_label === 2) {
-		this.func ();
 		return this.produced_output (0) ();
 
 
+	    } else {
+		this.panic ("try_self", _label); 
+	    }
+	},
+	no_output: function (_label) {
+	    if (_label === 0) {
+		return no_output (3);
+	    } else if (_label === 3) {
+		this.funcX ();
 
-		else {
-		    this.panic ("try_self", _label); 
-		}
-	    },
-	    no_output C: function (_label) {
-		if (_label === 0) {
-		    return no_output (3);
-		} else if (_label === 3) {
-		    this.funcX ();
 
+	    } else {
+		this.panic ("no_output", _label); 
+	    }
+	},
+	produced_output: function (_label) {
+	    if (_label === 0) {
+		this.funcY ();
 
-		    else {
-			this.panic ("no_output", _label); 
-		    }
-		},
-		produced_output B: function (_label) {
-		    if (_label === 0) {
-			this.funcY ();
-
-		    } else {
-			this.panic ("produced_output", _label); 
-		    }
-		},
-		_endoflambdas: null
-	    };
-	    return (function () { this.lambdas.Try_component (0); });
-	}
+	    } else {
+		this.panic ("produced_output", _label); 
+	    }
+	},
+	_endoflambdas: null
+    };
+    return (function () { this.lambdas.Try_component (0); });
+}
 
