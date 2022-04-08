519fd90be77e94faf960a02cccd740b30af84d28
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-08 17:31:34 -0400

    forEach for queue

--- queue.js
+++ queue.js
@@ -3,6 +3,7 @@ function Queue () {
     this.empty = function () { return (0 === this.queue.length) };
     this.enqueue = function (item) { this.queue.push (item); };
     this.dequeue = function () { return this.queue.pop (); };
+    this.forEach = function (f) { return this.queue.forEach (f); };
 }
 
 /* var q = new Queue ();
