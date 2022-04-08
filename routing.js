6f679e19323e6d793664c1362e07255017760912
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-08 17:34:57 -0400

    remove .this from call

--- routing.js
+++ routing.js
@@ -8,23 +8,23 @@ connection.lock ();
 var dest = connection.forEach (receiver => {
 var params = [_me, message, receiver];
 if (dest.component !== _me) {
-this.deliver_output_to_child_input (params);
+deliver_output_to_child_input (params);
 } else if (dest.component === _me) {
-this.deliver_output_to_me_output (params);
+deliver_output_to_me_output (params);
 }
 });
 connection.unlock ();
 });
-this.child.resetOutputQueue ();
+child.instance.resetOutputQueue ();
 });
 }
 
 this.deliver_output_to_child_input = function (_me, receiver, message) {
 var input_message = [receiver.etag, message.data];
-this.receiver.enqueueInputMessage (input_message);
+receiver.enqueueInputMessage (input_message);
 }
 
 this.deliver_output_to_me_output = function (_me, receiver, message) {
 var output_message = [receiver.etag, message.data];
-this._me.enqueueInputMessage (output_message);
+_me.enqueueInputMessage (output_message);
 }
