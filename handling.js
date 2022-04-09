a0f1ad5f95396b53ec74ef4b47cdfda0d7245572
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-09 17:37:49 -0400

    forevery ; return

--- handling.js
+++ handling.js
@@ -1,14 +1,17 @@
 exports.deliverInputMessageToAllChildrenOfSelf = function (message) {
 var _me = this;
+var _ret =  null;
+
 var connection = _me.find_connection (message.etag);
 connection.lock ();
-var dest = connection.forEach (receiver => {
+connection.dest.forEach (receiver => {
 var params = [_me, message, receiver];
-if (dest.component !== _me) {
+if ((dest.component !== _me)) {
 deliver_input_from_container_input_to_child_input (params);
-} else if (dest.component === _me) {
+} else if ((dest.component === _me)) {
 deliver_input_from_container_input_to_me_output (params);
 }
 });
 connection.unlock ();
+return  _ret;
 }
