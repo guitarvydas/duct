2d3c11d7fb24cc617daa59018cb7eba33eeb3670
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-09 19:05:50 -0400

    debug connection name

--- find_connection.js
+++ find_connection.js
@@ -3,13 +3,7 @@ var _me = this;
 var _ret =  null;
 
 _me.connections.forEach (connection => {
-var sender = connection.sender;
-
-console.log (sender.component);
-console.log ((sender.component === "$me"));
-console.log (sender.etag);
-console.log (etag);
-console.log ((sender.etag === etag))
+var sender = connection.sender.sender;
 
 if ((sender.component === "$me") && (sender.etag === etag)) {
 
