5cddced9a8de289cd59b7f82ebf133225eda5de0
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-10 05:39:55 -0400

    cp before this <- me

--- step.js
+++ step.js
@@ -1,83 +1,83 @@
 
 exports.Try_component = function () {
-var lambdas = {
-main: function (_me, _label) {
-if (_label === 0) {
-if (!_me.has_children ()) {
- return lambdas.try_self (_me, 1);
-} else {
+    var lambdas = {
+	main: function (_me, _label) {
+	    if (_label === 0) {
+		if (!_me.has_children ()) {
+		    return lambdas.try_self (_me, 1);
+		} else {
 
-_me.step_each_child ();
-if (!_me.child_produced_output ()) {
- return lambdas.try_self (_me, 2);
-} else {
+		    _me.step_each_child ();
+		    if (!_me.child_produced_output ()) {
+			return lambdas.try_self (_me, 2);
+		    } else {
 
-return lambdas.produced_output (_me, 0) ();
+			return lambdas.produced_output (_me, 0) ();
 
-;}
-
-
-;}
-
-} else {
-_me.panic ("main", _label); 
-}
-},
-try_self: function (_me, _label) {
-if (_label === 0) {
-return lambdas.try_self (_me, 1);
-} else if (_label === 1) {
-
-return lambdas.try_self (_me, 2);
-} else if (_label === 2) {
-_me.run_self ();
-if (!_me.self_produced_output ()) {
- return lambdas.no_output (_me, 3);
-} else {
-
-return lambdas.produced_output (_me, 0) ();
-
-;}
+			;}
 
 
+		    ;}
 
-} else {
-_me.panic ("try_self", _label); 
-}
-},
-no_output: function (_me, _label) {
-if (_label === 0) {
-return lambdas.no_output (_me, 3);
-} else if (_label === 3) {
-_me.send ("no_output", true);
-return lambdas.finished (_me, 0) ();
+	    } else {
+		_me.panic ("main", _label); 
+	    }
+	},
+	try_self: function (_me, _label) {
+	    if (_label === 0) {
+		return lambdas.try_self (_me, 1);
+	    } else if (_label === 1) {
+
+		return lambdas.try_self (_me, 2);
+	    } else if (_label === 2) {
+		_me.run_self ();
+		if (!_me.self_produced_output ()) {
+		    return lambdas.no_output (_me, 3);
+		} else {
+
+		    return lambdas.produced_output (_me, 0) ();
+
+		    ;}
 
 
 
-} else {
-_me.panic ("no_output", _label); 
-}
-},
-produced_output: function (_me, _label) {
-if (_label === 0) {
-_me.send ("produced_output", true);
-return lambdas.finished (_me, 0) ();
+	    } else {
+		_me.panic ("try_self", _label); 
+	    }
+	},
+	no_output: function (_me, _label) {
+	    if (_label === 0) {
+		return lambdas.no_output (_me, 3);
+	    } else if (_label === 3) {
+		_me.send ("no_output", true);
+		return lambdas.finished (_me, 0) ();
 
 
-} else {
-_me.panic ("produced_output", _label); 
-}
-},
-finished: function (_me, _label) {
-if (_label === 0) {
-_me.end ();
 
-} else {
-_me.panic ("finished", _label); 
-}
-},
-_endoflambdas: null
-};
-return (function (_me) { lambdas.main (_me, 0); });
+	    } else {
+		_me.panic ("no_output", _label); 
+	    }
+	},
+	produced_output: function (_me, _label) {
+	    if (_label === 0) {
+		_me.send ("produced_output", true);
+		return lambdas.finished (_me, 0) ();
+
+
+	    } else {
+		_me.panic ("produced_output", _label); 
+	    }
+	},
+	finished: function (_me, _label) {
+	    if (_label === 0) {
+		_me.end ();
+
+	    } else {
+		_me.panic ("finished", _label); 
+	    }
+	},
+	_endoflambdas: null
+    };
+    return (function (_me) { lambdas.main (_me, 0); });
 }
 
