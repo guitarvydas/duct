4487fb30a2791806be25bb8203e85004049a1a93
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-11 09:34:40 -0400

    destructuring parameters

--- routing.js
+++ routing.js
@@ -8,7 +8,7 @@ var message = output_message;
 var connection = this.find_connection_in__me (this, child, message.etag);
 
 connection.receivers.forEach (dest => {
-var params = [_me, message, dest];
+var params = [_me, dest, message];
 if ((dest.component !== _me)) {
 deliver_to_child_input (params);
 } else if ((dest.component === _me)) {
@@ -21,12 +21,12 @@ child.runnable.resetOutputQueue ();
 return _ret;
 }
 
-deliver_to_child_input = function (_me, receiver, message) {
+deliver_to_child_input = function ([_me, receiver, message]) {
 var input_message = [receiver.etag, message.data];
 receiver.enqueueInputMessage (input_message);
 }
 
-deliver_to_me_output = function (_me, receiver, message) {
+deliver_to_me_output = function ([_me, receiver, message]) {
 var output_message = [receiver.etag, message.data];
 _me.enqueueInputMessage (output_message);
 }
