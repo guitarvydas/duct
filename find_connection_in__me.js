83ecee316f79e173b721756face452ac73ef17a8
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-11 07:52:26 -0400

    why is etag undefined?

--- find_connection_in__me.js
+++ find_connection_in__me.js
@@ -1,17 +1,17 @@
-exports.find_connection_in__me = function (_me, childname, etag) {
+exports.find_connection_in__me = function (_me, childobj, etag) {
 var _ret =  null;
 
 _me.connections.forEach (connection => {
 var sender = connection.sender;
 
-if ((sender.component === childname) && (sender.etag === etag)) {
+if ((sender.component === childobj.name) && (sender.etag === etag)) {
 
 _ret = connection;
 }
 });
 if (_ret === null) {
 console.error ("no value returned");
-console.error (`find_connection_in__me: connection not found for ${childname}:${etag}`);
+console.error (`find_connection_in__me: connection not found for ${childobj.name}:${etag}`);
 process.exit (1);}
 
 return  _ret;
