b9a357881f689fb210817673203adfb66d2a8240
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-27 12:49:07 -0400

    unshift for enqueue

--- queue.js
+++ queue.js
@@ -1,7 +1,7 @@
 function Queue () {
     this.queue = [];
     this.empty = function () { return (0 === this.queue.length) };
-    this.enqueue = function (item) { this.queue.push (item); };
+    this.enqueue = function (item) { this.queue.unshift (item); };
     this.dequeue = function () { return this.queue.pop (); };
     this.forEach = function (f) { return this.queue.forEach (f); };
     this.length = function () { return this.queue.length; };
