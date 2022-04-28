94532b6e54fcbdb025c30222b09573f8b5d3a643
Paul Tarvydas <guitarvydas@gmail.com>
2022-04-28 12:59:16 -0400

    use is me and is not me in handling.das

--- handling.js
+++ handling.js
@@ -1,19 +1,19 @@
 const msg = require ('./message');
 exports.deliverInputMessageToAllChildrenOfSelf = function (_me, message) {
-var _ret =  null;
+    var _ret =  null;
 
-var connection = _me.find_connection (_me, message.etag);
-if (connection) {
+    var connection = _me.find_connection (_me, message.etag);
+    if (connection) {
 
-connection.receivers.forEach (dest => {
-var params = [_me, message, dest];
-if ((dest.name !== _me)) {
-_me.deliver_input_from_container_input_to_child_input (params);
-} else if ((dest.name === _me)) {
-_me.deliver_input_from_container_input_to_me_output (params);
-}
-});
-} else {
-}
-return  _ret;
+	connection.receivers.forEach (dest => {
+	    var params = [_me, message, dest];
+	    if ((dest.name !== "_me")) {
+		_me.deliver_input_from_container_input_to_child_input (params);
+	    } else if ((dest.name === "_me")) {
+		_me.deliver_input_from_container_input_to_me_output (params);
+	    }
+	});
+    } else {
+    }
+    return  _ret;
 }
